ecc2b0822d64a578e806b9d50b622dd0b76c78d8
harris <harrisleesh@gmail.com>
2020-03-28 00:10:49 +0900

    React Study#3
    ref, concat, filter

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import MyComponent from "./MyComponent";
 import Counter from "./Counter";
 import Say from "./Say";
 import EventPractice from "./EventPractice";
+import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
+import IterationSample from "./IterationSample";
 
-const App = () => {
-  return <EventPractice />;
-};
+class App extends Component {
+  render() {
+    return <IterationSample />;
+  }
+}
 
 export default App;
